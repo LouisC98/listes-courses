3a608f4891a7e819ef5dd211e0247a433163969f
CLouis98 <louis.carvalho98@gmail.com>
2026-04-13 15:05:30 +0200

    design v2 new icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Navbar} from './navbar/navbar';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, NgOptimizedImage],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
